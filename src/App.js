5a80a7c20f77446ef0d7a657ea7eddc7e18984fa
QuentinGruber <QuentinGruber@gmail.com>
2020-03-02 14:10:48 +0100

    Add Argon theme assets

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Login from './component/Login';
 import Register from './component/Register'
-
-
+import FormCreator from './component/FormCreator'
+import "./assets/css/argon-dashboard-react.css";
+import "./assets/css/argon-dashboard-react.min.css";
+import "./assets/css/argon-dashboard-react.css.map";
 
 
 function App() {
