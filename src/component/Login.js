b2aab0aac714d53001f77caab06a7c460ff1d313
undefined <undefined>
2020-03-03 13:17:26 +0100

    aragon login + register

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,5 +1,20 @@
 import React from 'react';
 
+import {
+    Button,
+    Card,
+    CardHeader,
+    CardBody,
+    FormGroup,
+    Form,
+    Input,
+    InputGroupAddon,
+    InputGroupText,
+    InputGroup,
+    Row,
+    Col
+  } from "reactstrap";
+  
 class Login extends React.Component {
     
     render() {
@@ -34,25 +49,105 @@ class Login extends React.Component {
         
         }
       return(
-        <form id="loginBox">
-        LOGIN SECTION
-        
-        <input id = "Login_name" name ="username" placeholder="Username"></input>
-        
 
-        
-        <input type="password" id ="Login_pass" name="password" placeholder="Password"></input>
-        
 
-        
-        <input type="checkbox" id="check_login" name="checkbox rester connecté"></input>
-        <label htmlFor="check_login">Rester connecté</label>
+<>
+        <Col lg="5" md="7">
+          <Card className="bg-secondary shadow border-0">
+            <CardHeader className="bg-transparent pb-5">
+              <div className="text-muted text-center mt-2 mb-3">
+                <small>Se connecter avec</small>
+              </div>
+              <div className="btn-wrapper text-center">
+                <Button
+                  className="btn-neutral btn-icon"
+                  color="default"
+                  href="#pablo"
+                  onClick={e => e.preventDefault()}
+                >
 
-        
-        <button onClick = {Login} type="button">SUBMIT</button>
-        </form>
+                  <span className="btn-inner--text">Github</span>
+                </Button>
+                <Button
+                  className="btn-neutral btn-icon"
+                  color="default"
+                  href="#pablo"
+                  onClick={e => e.preventDefault()}
+                >
 
-        
+                  <span className="btn-inner--text">Google</span>
+                </Button>
+              </div>
+            </CardHeader>
+            <CardBody className="px-lg-5 py-lg-5">
+              <div className="text-center text-muted mb-4">
+                <small>Ou se connecter avec les identifiants</small>
+              </div>
+              <Form role="form" id="loginBox">
+                <FormGroup className="mb-3">
+                  <InputGroup className="input-group-alternative">
+                    <InputGroupAddon addonType="prepend">
+                      <InputGroupText>
+                        <i className="ni ni-email-83" />
+                      </InputGroupText>
+                    </InputGroupAddon>
+                    <Input id="Login_name" name="username" placeholder="Username"/>
+                  </InputGroup>
+                </FormGroup>
+                <FormGroup>
+                  <InputGroup className="input-group-alternative">
+                    <InputGroupAddon addonType="prepend">
+                      <InputGroupText>
+                        <i className="ni ni-lock-circle-open" />
+                      </InputGroupText>
+                    </InputGroupAddon>
+                    <Input id="Login_pass" name="password" placeholder="Password" type="password"/>
+                  </InputGroup>
+                </FormGroup>
+                <div className="custom-control custom-control-alternative custom-checkbox">
+                  <input
+                    className="custom-control-input"
+                    id="check_login"
+                    type="checkbox"
+                    name="checkbox rester connecté"
+                  />
+                  <label
+                    className="custom-control-label"
+                    htmlFor="check_login"
+                  >
+                    <span className="text-muted">Rester connecté</span>
+                  </label>
+                </div>
+                <div className="text-center">
+                  <Button onClick = {Login} className="my-4" color="primary" type="button">
+                    Sign in
+                  </Button>
+                </div>
+              </Form>
+              <Row className="mt-3">
+                <Col xs="6">
+                  <a
+                    className="text-light"
+                    href="#pablo"
+                    onClick={e => e.preventDefault()}
+                  >
+                    <small>Forgot password?</small>
+                  </a>
+                </Col>
+                <Col className="text-right" xs="6">
+                  <a
+                    className="text-light"
+                    href="#pablo"
+                    onClick={e => e.preventDefault()}
+                  >
+                    <small>Create new account</small>
+                  </a>
+                </Col>
+              </Row>
+            </CardBody>
+          </Card>
+        </Col>
+      </>
       );
     }
   }
--- src/component/Register.js
+++ src/component/Register.js
@@ -1,5 +1,20 @@
 import React from 'react';
 
+import {
+    Button,
+    Card,
+    CardHeader,
+    CardBody,
+    FormGroup,
+    Form,
+    Input,
+    InputGroupAddon,
+    InputGroupText,
+    InputGroup,
+    Row,
+    Col
+  } from "reactstrap";
+  
 class Register extends React.Component {
     render() {
       function Get_AdminToken(){
@@ -94,6 +109,115 @@ class Register extends React.Component {
     }
     
       return (
+        <>
+        <Col lg="6" md="8">
+          <Card className="bg-secondary shadow border-0">
+            <CardHeader className="bg-transparent pb-5">
+              <div className="text-muted text-center mt-2 mb-4">
+                <small>S'inscrire avec</small>
+              </div>
+              <div className="text-center">
+                <Button
+                  className="btn-neutral btn-icon mr-4"
+                  color="default"
+                  href="#pablo"
+                  onClick={e => e.preventDefault()}
+                >
+                  <span className="btn-inner--text">Github</span>
+                </Button>
+                <Button
+                  className="btn-neutral btn-icon"
+                  color="default"
+                  href="#pablo"
+                  onClick={e => e.preventDefault()}
+                >
+                  <span className="btn-inner--text">Google</span>
+                </Button>
+              </div>
+            </CardHeader>
+            <CardBody className="px-lg-5 py-lg-5">
+              <div className="text-center text-muted mb-4">
+                <small>Ou s'inscrire avec les identifiants</small>
+              </div>
+              <Form role="form" id="registerBox">
+                <FormGroup>
+                  <InputGroup className="input-group-alternative mb-3">
+                    <InputGroupAddon addonType="prepend">
+                      <InputGroupText>
+                        <i className="ni ni-hat-3" />
+                      </InputGroupText>
+                    </InputGroupAddon>
+                    <Input id = "Register_name" name ="username"placeholder="Username" type="text" pattern = '.{3,16}' required/>
+                  </InputGroup>
+                </FormGroup>
+                <FormGroup>
+                  <InputGroup className="input-group-alternative mb-3">
+                    <InputGroupAddon addonType="prepend">
+                      <InputGroupText>
+                        <i className="ni ni-email-83" />
+                      </InputGroupText>
+                    </InputGroupAddon>
+                    <Input type = "text" id = "Register_email" name ="email" placeholder = "Email"  pattern = '^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$' title = "Doit respecter le format d'un email" required/>
+                    </InputGroup>
+                </FormGroup>
+                <FormGroup>
+                  <InputGroup className="input-group-alternative">
+                    <InputGroupAddon addonType="prepend">
+                      <InputGroupText>
+                        <i className="ni ni-lock-circle-open" />
+                      </InputGroupText>
+                    </InputGroupAddon>
+                    <Input type = "password" id = "Register_pass" name = "password" placeholder = "Password" pattern = "(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title = "Doit contenir au moins un chiffre, une minuscule, une majuscule, et 8 caractères" autoComplete="new-password" required/>
+                  </InputGroup>
+                </FormGroup>
+                <Row className="my-4">
+                  <Col xs="12">
+                    <div className="custom-control custom-control-alternative custom-checkbox">
+                      <input
+                        className="custom-control-input"
+                        id="check_register"
+                        type="checkbox"
+                        name="checkbox inscription"
+                        required
+                      />
+                      <label
+                        className="custom-control-label"
+                        htmlFor="check_register">
+                      
+                        <span className="text-muted">
+                          J'accepte les {" "}
+                          <a href="https://www.dofus.com/fr" >
+                            conditions d'utilisation
+                          </a>
+                            ESPACE de Online Survey
+                        </span>
+                      </label>
+                    </div>
+                  </Col>
+                </Row>
+                <div className="text-center">
+                  <Button onClick={Verify_register_info} id="registerSubmitButton" className="mt-4" color="primary" type="button"value="SUBMIT">
+                    S'inscrire
+                  </Button>
+                </div>
+              </Form>
+            </CardBody>
+          </Card>
+        </Col>
+        </>
+
+
+
+
+
+
+
+
+
+
+
+
+        /*
         <form id="registerBox">
             REGISTER SECTION
             
@@ -116,6 +240,7 @@ class Register extends React.Component {
             
             <input value="SUBMIT" type="button" onClick={Verify_register_info} id="registerSubmitButton" ></input>
         </form>
+        */
       )
     }
   }
