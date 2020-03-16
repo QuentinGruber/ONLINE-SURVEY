af4438b0dc440115b809e3f976a8e916253932d9
QuentinGruber <QuentinGruber@gmail.com>
2020-03-16 16:58:13 +0100

    add twitter login package

---
+++ src/component/sub_component/twitter_login.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class twitter_login extends Component {
+  static defaultProps = {}
+  static propTypes = {}
+
+  render() {
+    return (
+      <div className="twitter_login">
+        twitter_login
+      </div>
+    )
+  }
+}
+
+export default twitter_login
