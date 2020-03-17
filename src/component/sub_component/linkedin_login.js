65aca02623834c0edf7b8311f22cd66975a6e1bb
QuentinGruber <QuentinGruber@gmail.com>
2020-03-17 12:26:11 +0100

    test new package

--- src/component/sub_component/linkedin_login.js
+++ src/component/sub_component/linkedin_login.js
@@ -1,33 +1,32 @@
 import React from 'react';
-import { LinkedIn } from 'react-linkedin-login-oauth2';
+import LinkedIn from "linkedin-login-for-react";
+import styles from "./styles.css";
 
 
 
 class LinkedInLogin extends React.Component {
-    handleSuccess = (data) => {
-        this.setState({
-          code: data.code,
-          errorMessage: '',
-        });
-        console.log(data)
-      }
-    
-      handleFailure = (error) => {
-        this.setState({
-          code: '',
-          errorMessage: error.errorMessage,
-        });
-      }
+    static propTypes = {};
+ 
+  /*
+    ** @code = authorization code from linkedin api
+    ** @redirectUri = redirect uri from linkedin api
+    ** @error = error message sign in failed
+    */
+  callbackLinkedIn = (error, code="KBghXMRTiKoNycwP", redirectUri="https://www.online-survey.app/auth/linkedin") => {
+    if (error) {
+      // signin failed
+    } else {
+      // Obtain authorization token from linkedin api
+      // see https://developer.linkedin.com/docs/oauth2 for more info
+    }
+  };
     render() {
         return (
             <LinkedIn
                 clientId="78s03bsfw6m7c3"
-                onFailure={this.handleFailure}
-                onSuccess={this.handleSuccess}
-                redirectUri="/"
-                renderElement={({ onClick, disabled }) => (
-                    <button onClick={onClick} disabled={disabled}>Custom linkedin element</button>
-                )}
+                callback={this.callbackLinkedIn}
+                className={styles.linkedin}
+                text="Login With LinkedIn"
             />
         )
     }
