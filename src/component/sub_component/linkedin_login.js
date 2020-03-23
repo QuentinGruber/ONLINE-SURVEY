6d3319690f86bd9c5ec6e0da2218115238b0fb45
QuentinGruber <QuentinGruber@gmail.com>
2020-03-23 21:39:06 +0100

    create linkedin connection fix scope/redirect url

--- src/component/sub_component/linkedin_login.js
+++ src/component/sub_component/linkedin_login.js
@@ -1,33 +1,45 @@
 import React from 'react';
-import LinkedIn from "linkedin-login-for-react";
+import LinkedIn from "react-linkedin-login-oauth2";
 import styles from "./styles.css";
 
 
 
 class LinkedInLogin extends React.Component {
-    static propTypes = {};
- 
-  /*
-    ** @code = authorization code from linkedin api
-    ** @redirectUri = redirect uri from linkedin api
-    ** @error = error message sign in failed
-    */
-  callbackLinkedIn = (error, code="KBghXMRTiKoNycwP", redirectUri="https://www.online-survey.app/auth/linkedin") => {
-    if (error) {
-      // signin failed
-    } else {
-      // Obtain authorization token from linkedin api
-      // see https://developer.linkedin.com/docs/oauth2 for more info
-    }
+  state = {
+    code: '',
+    errorMessage: '',
   };
+
+
+  handleSuccess = (data) => {
+    console.log(data)
+    this.setState({
+      code: data.code,
+      errorMessage: '',
+    });
+  }
+
+  handleFailure = (error) => {
+    console.log(error)
+    this.setState({
+      code: '',
+      errorMessage: error.errorMessage,
+    });
+  }
     render() {
+      const { code, errorMessage } = this.state;
+
         return (
-            <LinkedIn
-                clientId="78s03bsfw6m7c3"
-                callback={this.callbackLinkedIn}
-                className={styles.linkedin}
-                text="Login With LinkedIn"
-            />
+          <LinkedIn
+          scope="r_liteprofile+r_emailaddress"
+          clientId="78s03bsfw6m7c3"
+          onFailure={this.handleFailure}
+          onSuccess={this.handleSuccess}
+          redirectUri="http://localhost:3000/linkedin"
+          renderElement={({ onClick, disabled }) => (
+            <button onClick={onClick} disabled={disabled}>linkedin</button>
+          )}
+        />
         )
     }
 }
