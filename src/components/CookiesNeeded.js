8ded88f049f4578241e56847cc5775281002c399
Maxime <maximebier67@gmail.com>
2020-04-06 16:38:09 +0200

    style cookies (largeur) + corrections

--- src/components/CookiesNeeded.js
+++ src/components/CookiesNeeded.js
@@ -16,10 +16,10 @@ class CookiesNeeded extends React.Component {
   render() {
     return (
         <div style={{textAlign: "center"}}>
-        <h1>Ce site web à besoin des cookies pour fonctionner ! </h1>
-        <p style={{width: "50%",margin: "auto"}}>En poursuivant votre navigation, vous acceptez l’utilisation de cookies pour améliorer votre expérience sur notre site et/ou la fourniture des services proposés par Online Survey.
-         Ces types de cookies ne sont déposés sur vos terminaux qu’à condition que vous y consentiez,sur le site Internet ou l’application mobile de Online Survey. 
-          Consultez les <a onClick={this.DisplayLegal} href="#legal">Mentions Légales relatives à la RGPD</a> pour en savoir plus</p>
+        <h1>Ce site web a besoin des cookies pour fonctionner ! </h1>
+        <p style={{width: "90%",margin: "auto", textAlign: "justify"}}>En poursuivant votre navigation, vous acceptez l’utilisation de cookies pour améliorer votre expérience sur notre site et/ou la fourniture des services proposés par Online Survey.
+         Ces types de cookies ne sont déposés sur vos terminaux qu’à condition que vous y consentiez, sur le site Internet ou l’application mobile de Online Survey. 
+          Consultez les <a onClick={this.DisplayLegal} href="#legal">Mentions Légales relatives à la RGPD</a> pour en savoir plus.</p>
         <Button onClick={this.AcceptCookies}> Accepter les cookies </Button>
         </div>
     )
