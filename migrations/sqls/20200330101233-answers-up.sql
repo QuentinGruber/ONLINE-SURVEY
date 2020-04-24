d929160e9a91b4f90888f7643d1c4a5af50fc3df
Aurelied <280120@supinfo.com>
2020-04-24 09:41:37 +0200

    checked column for answers

--- migrations/sqls/20200330101233-answers-up.sql
+++ migrations/sqls/20200330101233-answers-up.sql
@@ -2,5 +2,6 @@
 CREATE TABLE `answers` (
   `id` bigint(20) UNSIGNED NOT NULL ,
   `question_id` bigint(20) UNSIGNED NOT NULL,
-  `text` varchar(255) NOT NULL
+  `text` varchar(255) NOT NULL,
+  `checked` tinyint(1)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
