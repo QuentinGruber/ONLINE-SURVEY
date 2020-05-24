54a0664e6ba756b70e78f57b0c36a0c1b56329aa
Kent1 <gruberquentin67@gmail.com>
2020-05-24 13:22:23 +0200

    remove useless form

--- src/components/FormApp/sub_components/NewQuestion.js
+++ src/components/FormApp/sub_components/NewQuestion.js
@@ -3,22 +3,21 @@ import React from "react";
 class NewQuestion extends React.Component {
   constructor(props) {
     super(props);
-    this.onSubmit = this.onSubmit.bind(this);
+    this.CreateItem = this.CreateItem.bind(this);
   }
-  onSubmit(event) {
-    event.preventDefault();
+  CreateItem() {
     // add new item
     let newItemValue = "";
     this.props.addItem({ newItemValue });
-    this.refs.NewItemForm.reset(); // reset input value after adding new item
   }
   render() {
     return (
-      <form ref="NewItemForm" onSubmit={this.onSubmit} className="form-inline">
-        <button type="submit" className="btn btn-secondary button-add">
-          +
-        </button>
-      </form>
+      <button
+        onClick={this.CreateItem()}
+        className="btn btn-secondary button-add"
+      >
+        +
+      </button>
     );
   }
 }
