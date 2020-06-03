22a6babc40cacefa948be98d427474d8e2933cfa
Kent1 <gruberquentin67@gmail.com>
2020-06-03 19:09:34 +0200

    everything seems to work :D

--- src/components/FormApp/index.js
+++ src/components/FormApp/index.js
@@ -28,7 +28,7 @@ class FormApp extends React.Component {
   }
 
   async saveItem() {
-    if (this.state.formitems.length != 0) {
+    if (this.state.formitems.length != 0 && this.state.FormName != "") {
       if (this.isNew) {
         // if this is a new question_list
         // register it
@@ -59,7 +59,9 @@ class FormApp extends React.Component {
               let delete_item_promise = await Axios({
                 method: "delete",
                 url: process.env.REACT_APP_API_URL + "/form_item/",
+                withCredentials: true,
                 data: {
+                  FormID: this.FormID,
                   id: this.ItemsToDelete[i],
                 },
               });
@@ -69,7 +71,7 @@ class FormApp extends React.Component {
               url: process.env.REACT_APP_API_URL + "/editform/",
               withCredentials: true,
               data: {
-                id: this.FormID,
+                FormID: this.FormID,
                 title: this.state.FormName,
                 content: this.state.formitems,
               },
@@ -85,7 +87,7 @@ class FormApp extends React.Component {
         }
       }
     } else {
-      alert("A form need at least one question to exist !");
+      alert("A form need at least a name & one question to exist !");
     }
   }
 
