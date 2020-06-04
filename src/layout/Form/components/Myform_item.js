cb47b46ea566f87c4cdf48604a6e7d74144d513d
Maxime <maximebier67@gmail.com>
2020-06-04 16:14:07 +0200

    add edit and stats divs

--- src/layout/Form/components/Myform_item.js
+++ src/layout/Form/components/Myform_item.js
@@ -4,8 +4,10 @@ import { Card, Button } from "reactstrap";
 class FormTitle extends React.Component {
   render() {
     return (
-      <Card id="form-list-card-form">
-        <div id="form-list-title">{this.props.data.name}</div>
+      <Card className="form-list-card-form">
+        <div className="form-list-title">{this.props.data.name}</div>
+        <div className="div-edit-form">Éditer le formulaire</div>
+        <div className="div-stats-form">Résultats et statistiques</div>
       </Card>
     );
   }
