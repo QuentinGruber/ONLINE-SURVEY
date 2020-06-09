25583b38ca9dbef7a4bbcbf4db343e29d9320972
Kent1 <gruberquentin67@gmail.com>
2020-06-09 15:43:48 +0200

    linecharts

--- src/components/ChartComponents/LineChart.js
+++ src/components/ChartComponents/LineChart.js
@@ -6,22 +6,44 @@ export default class LineChart extends Component {
 
   componentDidMount() {
     const myChartRef = this.chartRef.current.getContext("2d");
+    var randomHexColor = require("random-hex-color");
+    let color_array = [];
+    let label_array = [];
+    let data_array = [];
+    var count = require("count-array-values");
+    let data = count(this.props.data);
+    data.forEach((element) => {
+      color_array.push(randomHexColor());
+      label_array.push("Nombre d'entrées " + element.value);
+      data_array.push(element.count);
+    });
 
     new Chart(myChartRef, {
       type: "line",
       data: {
         //Bring in data
-        labels: this.props.data_date,
+        labels: label_array,
         datasets: [
           {
-            label: this.props.datalabel,
-            data: this.props.data,
-            backgroundColor: this.props.color,
+            fill: false,
+            data: data_array,
+            backgroundColor: color_array,
           },
         ],
       },
       options: {
-        //Customize chart options
+        legend: {
+          display: false,
+        },
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+            },
+          ],
+        },
       },
     });
   }
