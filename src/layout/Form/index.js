cb0fef2d20c2314155ee85c90e1459b3b38b29a1
Quentin Gruber <quentingruber@gmail.com>
2020-06-10 17:48:06 +0200

    form are now sorted by desc

--- src/layout/Form/index.js
+++ src/layout/Form/index.js
@@ -68,7 +68,7 @@ class Form extends React.Component {
       if (myform_list_promise.data !== false) {
         let items = [];
         for (let i = 0; i < myform_list_promise.data.length; i++) {
-          items.push(
+          items.unshift(
             <MyFormItem
               key={i}
               idx={i}
