8041d6a1674ee07a83d38c607b3bc2b4f016ed23
Quentin Gruber <quentingruber@gmail.com>
2020-06-11 22:36:43 +0200

    :mag:

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -24,6 +24,23 @@ class Welcome extends React.Component {
           <Link to="/auth/register">
             <Button>Inscription</Button>
           </Link>
+          <h2>Your forms, made simple</h2>
+          <p>
+            On sait, on sait : recevoir du feedback, c’est une des choses les
+            plus importantes pour votre entreprise. Formulaires de satisfaction
+            client, sondages auprès de vos collaborateurs, questionnaires
+            d’évaluations, fiches de renseignements… On pourrait étendre cette
+            liste à l’infini. Le temps passé à construire ces formulaires, en
+            extraire les données, les compiler sous formes de graphiques ou de
+            statistiques pourrait bien constituer la majeure partie de votre
+            emploi du temps. Et c’est là qu’on a quelque chose à vous proposer :
+            Online Survey, c’est tout d’abord une solution conçue pour vous
+            faire gagner du temps. Toutes les étapes pour recevoir du Feedback
+            en une seule application Web d’une simplicité déconcertante.
+            Connectez-vous, créez votre formulaire, recevez vos réponses,
+            visualisez vos résultats. Nous pensons que cela ne devrait pas être
+            plus compliqué que ça. Et avec Online Survey, ça ne l’est pas.
+          </p>
         </div>
         {/* footer*/}
         <div></div>
