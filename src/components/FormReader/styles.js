94c89ab7a94ed906c87ede7a1e407581caffcecf
Maxime <maximebier67@gmail.com>
2020-06-12 13:42:42 +0200

    fix ugly checkbox

--- src/components/FormReader/styles.js
+++ src/components/FormReader/styles.js
@@ -156,8 +156,8 @@ export const GlobalStyle = createGlobalStyle`
         border: 2px solid var(--active-inner);
         border-top: 0;
         border-left: 0;
-        left: 7px;
-        top: 4px;
+        left: 6px;
+        top: 3px;
         transform: rotate(var(--r, 20deg));
       }
       &:checked {
