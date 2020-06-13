8f9d8ae2ab64a6f9021c4b585eb5c358fdbb31b0
Quentin Gruber <quentingruber@gmail.com>
2020-06-13 22:10:33 +0200

    test serviceworker

--- src/index.js
+++ src/index.js
@@ -76,4 +76,4 @@ if (localStorage.AcceptCookies === "true") {
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
