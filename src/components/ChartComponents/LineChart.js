20462ad6b71761ebee2fba292ad266b7afec775c
Quentin Gruber <quentingruber@gmail.com>
2020-06-22 14:39:21 +0200

    sort linechart

--- src/components/ChartComponents/LineChart.js
+++ src/components/ChartComponents/LineChart.js
@@ -12,6 +12,11 @@ export default class LineChart extends Component {
     let data_array = [];
     var count = require("count-array-values");
     let data = count(this.props.data);
+    data.sort((a, b) => {
+      if (a.value < b.value) return -1;
+      if (a.value > b.value) return 1;
+      return 0;
+    });
     data.forEach((element) => {
       color_array.push(randomHexColor());
       label_array.push(element.value);
@@ -25,6 +30,7 @@ export default class LineChart extends Component {
         labels: label_array,
         datasets: [
           {
+            order: 1,
             fill: false,
             data: data_array,
             backgroundColor: color_array,
